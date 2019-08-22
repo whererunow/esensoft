1cfa4faae1627051abe626798f64fc634a83eb44
yangk <yangk@LAPTOP-QNG4KQ8E>
2019-08-22 15:13:20 +0800

    成绩信息导出实现

--- src/main/java/com/esen/abistudy/action/ActionWeekTest02.java
+++ src/main/java/com/esen/abistudy/action/ActionWeekTest02.java
@@ -1,15 +1,20 @@
 package com.esen.abistudy.action;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.Serializable;
+import java.io.OutputStreamWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.postgresql.translation.messages_zh_TW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +25,16 @@ import com.esen.abistudy.Server;
 import com.esen.abistudy.dao.ScoreRepository;
 import com.esen.abistudy.orm.entity.Result;
 import com.esen.abistudy.orm.entity.ScoreEntity;
-import com.esen.ecore.repository.PageRequest;
-import com.esen.ecore.util.jdbc.ArrayRowHandler;
 import com.esen.ecore.util.jdbc.RowHandler;
+import com.esen.util.FileFunc;
 import com.esen.util.StmFunc;
 import com.esen.util.UNID;
+import com.esen.util.exp.Expression;
+import com.esen.util.i18n.I18N;
 import com.esen.vfs2.Vfs2;
 import com.esen.vfs2.VfsFile2;
 import com.esen.vfs2.VfsOperator;
+
 /**
  * 第二周作业的action
  * @author yangk
@@ -41,37 +48,40 @@ public class ActionWeekTest02 {
 	 */
 	@Autowired
 	private Server server;
+
 	/**
 	 * 注入scoreRepository对象
 	 */
 	@Autowired
 	private ScoreRepository scoreRepository;
+
 	/**
 	 * 上传文件到vfs
+	 * @param file  用于接收上传的文件对象
+	 * @return 操作结果
 	 */
 	@RequestMapping("/uploadToVfs")
 	@ResponseBody
 	public Result uploadToVfs(MultipartFile file) {
 		try {
-			//出现异常时 返回操作失败的结果信息以及异常信息
-			//int i = 1/0;
 			Vfs2 vfs = server.getVfs();
 			VfsOperator operator = server.getVfsOperatorAsAdmin();
-			VfsFile2 import_dir = vfs.getVfsFile("/import", operator);
+			VfsFile2 importDir = vfs.getVfsFile("/import", operator);
 			//如果 /import 目录不存在则创建
-			import_dir.ensureExists(true);
+			importDir.ensureExists(true);
 			// 在import目录下导入上传文件，
 			InputStream is = file.getInputStream();
-			//第一个参数为文件的流对象 第二个参数为文件名 这个方法在文件已存在时会覆盖已有文件
-			import_dir.importStm(is, file.getOriginalFilename());	
-			return new Result(true, "操作成功");
+			importDir.importStm(is, file.getOriginalFilename());
+			return new Result(true, I18N.getString("com.esen.abistudy.action.actionweektest02.successmessage", "操作成功"));
 		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "操作失败  错误信息：" + e.getMessage());
-		}		
+			//出现异常时 返回操作失败的结果信息以及异常信息
+			return new Result(false, I18N.getString("com.esen.abistudy.action.actionweektest02.failmessage", "操作失败  错误信息：" + e.getLocalizedMessage()));
+		}
 	}
+
 	/**
 	 * 将dataImport.txt导入到数据库表score中
+	 * @return 操作结果
 	 */
 	@RequestMapping("/importToDb")
 	@ResponseBody
@@ -87,12 +97,12 @@ public class ActionWeekTest02 {
 			String string = StmFunc.readString(is, file.getCharset());
 			//根据空行进行分割 得到每个月份的成绩信息字符串
 			String[] strings = string.split("\n\r\n");
-			for(int i = 0; i< strings.length;i++) {
+			for (int i = 0; i < strings.length; i++) {
 				//根据换行符进行拆分  得到每一行的字符串
 				String[] lines = strings[i].split("\n");
 				//获取第一行的日期字符串 yyyyMMdd格式
 				String date = lines[0].split("：")[1].replace("-", "").trim();
-				for(int j=2; j<lines.length; j++) {
+				for (int j = 2; j < lines.length; j++) {
 					//获取成绩信息 循环插入数据库
 					String[] line = lines[j].trim().split(",");
 					String student = line[0];
@@ -103,45 +113,107 @@ public class ActionWeekTest02 {
 					entity.setDate(date);
 					entity.setStudent(student);
 					entity.setCourse(course);
-					entity.setScore(Float.parseFloat(score));					
+					entity.setScore(Float.parseFloat(score));
 					scoreRepository.add(entity);
 				}
 			}
-			return new Result(true, "操作成功");
+			return new Result(true, I18N.getString("com.esen.abistudy.action.actionweektest02.successmessage", "操作成功"));
 		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "操作失败  错误信息：" + e.getMessage());
+			return new Result(false,I18N.getString("com.esen.abistudy.action.actionweektest02.failmessage", "操作失败  错误信息：" + e.getLocalizedMessage()));
 		}
 	}
+
 	/**
 	 * 查询成绩
 	 * @param req 用来往ftl页面传递数据
-	 * @param dateIndex  日期列表的索引
-	 * @return
+	 * @param dateIndex  当前日期在日期列表中的索引
+	 * @return 操作结果
 	 */
 	@RequestMapping("/queryScore")
-	public String queryScore(HttpServletRequest req,Integer dateIndex) {
+	public String queryScore(HttpServletRequest req, Integer dateIndex) {
+		//获取日期下拉列表 转换为页面展示所需格式
+		List<String> list = getDateList();
+		List<String> dateList = new ArrayList<>();
+		for (String dateStr : list) {
+			String date = dateStr.substring(0, 4) + "年" + dateStr.substring(4, 6) + "月";
+			dateList.add(date);
+		}
+		req.setAttribute("dateList", dateList);
+		//查询成绩列表 传递到ftl页面
+		List<HashMap<String, Object>> scoreList = getScoreList(list, dateIndex);
+		req.setAttribute("scoreList", scoreList);
+		//将选中的日期索引也传递给ftl页面，保证下拉框中对应日期的option被选中
+		req.setAttribute("dateIndex", dateIndex);
+		return "abistudy/score";
+	}
+
+	/**
+	 * 将成绩信息导出
+	 * @param dateIndex 当前日期在日期列表中的索引
+	 * @return 操作结果
+	 */
+	@RequestMapping("/exportScore")
+	@ResponseBody
+	public Result exportScore(Integer dateIndex) {
+		try {
+			//获取日期下拉列表
+			List<String> list = getDateList();
+			//获取成绩列表
+			List<HashMap<String, Object>> scoreList = getScoreList(list, dateIndex);
+			String date = list.get(dateIndex).substring(0, 6);
+			//文件名为dataExport_yyyyMM.txt格式
+			File file = new File("D:\\dataExport_" + date + ".txt");
+			FileFunc.ensureExists(file, false, true);
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+			try {
+				//向文件中写入成绩信息
+				bw.write("月份：" + date.substring(0, 4) + "年" + date.substring(4) + "月");
+				bw.newLine();
+				bw.write("姓名,语文,数学,外语,物理,化学,总分");
+				for (HashMap<String, Object> map : scoreList) {
+					bw.newLine();
+					bw.write(map.get("STUDENT_").toString() + "," + map.get("语文").toString() + "," + map.get("数学").toString()
+							+ "," + map.get("外语").toString() + "," + map.get("物理").toString() + "," + map.get("化学").toString() + ","
+							+ map.get("总分").toString());
+				}
+			} finally {
+				bw.close();
+			}
+			return new Result(true, I18N.getString("com.esen.abistudy.action.actionweektest02.successmessage", "操作成功"));
+		} catch (Exception e) {
+			return new Result(false, I18N.getString("com.esen.abistudy.action.actionweektest02.failmessage", "操作失败  错误信息：" + e.getLocalizedMessage()));
+		}
+	}
+
+	/**
+	 * 获取日期下拉列表
+	 * @return
+	 */
+	private List<String> getDateList() {
 		RowHandler<String> handler = new RowHandler<String>() {
 			@Override
 			public String processRow(ResultSet resultset) throws SQLException {
 				return resultset.getString(1);
 			}
 		};
-		//查询月份列表 转换为页面需求格式传递到ftl页面
-		List<String> list = scoreRepository.queryAll(
-				"select distinct date_ from score t order by date_ desc", handler, null);
-		List<String> dateList = new ArrayList<>();
-		for (String dateStr : list) {
-			String date= dateStr.substring(0, 4)+"年"+dateStr.substring(4,6)+"月";
-			dateList.add(date);
-		}
-		req.setAttribute("dateList", dateList);
-		//查询成绩列表 传递到ftl页面
+		List<String> list = scoreRepository.queryAll("select distinct date_ from score t order by date_ desc", handler,
+				null);
+		return list;
+	}
+
+	/**
+	 * 根据日期获取成绩列表
+	 * @param list  日期列表
+	 * @param dateIndex  当前日期在日期列表中的索引
+	 * @return 成绩列表 List中装map的形式
+	 */
+	private List<HashMap<String, Object>> getScoreList(List<String> list, Integer dateIndex) {
 		List<HashMap<String, Object>> scoreList = scoreRepository.queryForMapList(
 				"select student_,语文,数学,外语,物理,化学,语文+数学+外语+物理+化学 as 总分"
-				+ " from (select * from (select student_,course_,score_ from score where date_ = '"+list.get(dateIndex)+"') "
-				+ " pivot(max(score_) for course_ in ('语文' as 语文,'数学' as 数学,'外语' as 外语,'物理' as 物理,'化学' as 化学))) order by 总分 DESC", null);
-		req.setAttribute("scoreList", scoreList);
-		return "abistudy/score";
+						+ " from (select * from (select student_,course_,score_ from score where date_ = '" + list.get(dateIndex)
+						+ "') "
+						+ " pivot(max(score_) for course_ in ('语文' as 语文,'数学' as 数学,'外语' as 外语,'物理' as 物理,'化学' as 化学))) order by 总分 DESC",
+				null);
+		return scoreList;
 	}
 }
--- src/main/resources/META-INF/resources/abistudy/score.ftl
+++ src/main/resources/META-INF/resources/abistudy/score.ftl
@@ -19,43 +19,96 @@
   	width:800px;
 		border-collapse: collapse;
   }  
+  select{
+  	width:200px;
+  	height:22px;
+  	border: solid 1px gray;
+  	appearance:none;
+		-moz-appearance:none;
+		-webkit-appearance:none;
+		background: url("http://ourjs.github.io/static/2015/arrow.png") no-repeat scroll right center transparent;
+		padding-right: 14px;
+  }
+  button{
+  	padding:4px 40px;
+  	border-radius: 5px; 
+  	background-color: #DDDDDD; 
+  	border: 1px  #555 solid; 
+  	color: #333;
+  }	
 </style>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script>
+$(document).ready(function(){
+	 <#-- 当日期下拉框发生改变时 查询当前月份的成绩信息 -->
+	 $("#selectDate").change(function(){
+	 		var dateIndex = $("#selectDate").val();
+	 		location.href="queryScore?dateIndex="+dateIndex;
+	 })
+	 <#-- 当页面重新加载时 保证日期下拉框中的选定值不变 -->
+	 $.each($("#selectDate option"),function(){
+	 	if($(this).attr("value") == ${dateIndex}){
+	 		$(this).prop("selected",true);
+	 	}
+	 })
+	 <#-- 根据成绩的不同显示对应的颜色 -->
+	 $.each($(".score"),function(){
+	 	if($(this).text()>=90){
+	 		$(this).css("background","#009900");
+	 		$(this).css("color","white");
+	 	}
+	 	if($(this).text()>=60 && $(this).text()<70){
+	 		$(this).css("background","#FFFF00");
+	 		$(this).css("color","black");
+	 	}
+	 	if($(this).text()<60){
+	 		$(this).css("background","#FF0000");
+	 		$(this).css("color","white");
+	 	}
+	 })
+	 
+	 $("#export").click(function(){
+	 	$.post("/abistudy/actionWeekTest02/exportScore",{dateIndex:${dateIndex}},function(response){
+			location.href = "/abistudy/abistudy/result.html?message=" + response.message;
+		})
+	 })
+})
+</script>
 </head>
 <body>
 	<div id="main">
 		<div style="border-bottom:solid 1px gray;height:30px;text-align:center">成绩查询</div>
 		<div style="border-bottom:solid 1px gray;margin-bottom:-1px;height:30px">
 			<span style="margin-left:10px">
-				<select>
+				<select id="selectDate">
 				<#list dateList as date>
 					<option value = ${date_index}>${date}</option>
 				</#list>
 				</select>
 			</span>
-			<span style="margin-left:630px">
-				<button> 导出 </button>
+			<span style="margin-left:470px;margin-top:5px">
+				<button id="export"> 导出  </button>
 			</span>
 		</div>
 		<div>
 			<table>
 				<tr style="text-align:center;background:#D3D3D3">
-					<td>姓名</td>
-					<td>语文</td>
-					<td>数学</td>
-					<td>外语</td>
-					<td>物理</td>
-					<td>化学</td>
-					<td>总分</td>
+					<td><strong>姓名</strong></td>
+					<td><strong>语文</strong></td>
+					<td><strong>数学</strong></td>
+					<td><strong>外语</strong></td>
+					<td><strong>物理</strong></td>
+					<td><strong>化学</strong></td>
+					<td><strong>总分</strong></td>
 				</tr>
 				<#list scoreList as score>
 				<tr>
 					<td>${score.STUDENT_}</td>
-					<td style="text-align:right">${score.语文}</td>
-					<td style="text-align:right">${score.数学}</td>
-					<td style="text-align:right">${score.外语}</td>
-					<td style="text-align:right">${score.物理}</td>
-					<td style="text-align:right">${score.化学}</td>
+					<td class="score" style="text-align:right">${score.语文}</td>
+					<td class="score" style="text-align:right">${score.数学}</td>
+					<td class="score" style="text-align:right">${score.外语}</td>
+					<td class="score" style="text-align:right">${score.物理}</td>
+					<td class="score" style="text-align:right">${score.化学}</td>
 					<td style="text-align:right">${score.总分}</td>
 				</tr>
 				</#list>
